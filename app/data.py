1c6976a5fd4a8ccccb9f7625c0a8b11d2d2d2954
Caleb <calebrotich10@gmail.com>
2018-09-06 05:54:50 +0300

    [feature #160262021] Add data structure for storing orders

---
+++ app/data.py
@@ -0,0 +1,17 @@
+def Orders():
+    orders = [
+        {
+            "order_id" : 1,
+            "food_ordered": "Fries",
+            "quantity" : 4,
+            "date_ordered" : "05/09/2018"
+        },
+        {
+            "order_id" : 2,
+            "food_ordered": "Burger",
+            "quantity" : 5,
+            "date_ordered" : "04/09/2018"
+        }
+    ]
+
+    return orders
