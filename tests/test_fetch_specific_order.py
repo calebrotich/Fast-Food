e0b6d91221650940a5191a17e584984601d879a5
Caleb <calebrotich10@gmail.com>
2018-09-06 14:52:41 +0300

    [chore #] Add environment setup files

---
+++ tests/test_fetch_specific_order.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('C:\\Andela\\Fast-Food')
+import pytest
+from flask import json
+from app import app
+from app.data import Orders
+
+Orders = Orders()
+first_key = list(Orders.keys())[0]
+
+
+""" Test GET all orders """
+
+def test_fetch_specific_order():
+    result = app.test_client()
+    response = result.get('/api/v1/orders/'+first_key+'/', content_type='application/json')
+    assert(response.status_code == 200)
