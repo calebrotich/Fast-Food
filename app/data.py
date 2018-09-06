ea152b3fd44cbd7c3c3c9814743fe8016b8578ad
Caleb <calebrotich10@gmail.com>
2018-09-06 14:51:49 +0300

    [chore #160266978] Add dictionary to store orders

--- app/data.py
+++ app/data.py
@@ -1,17 +1,19 @@
 def Orders():
-    orders = [
+    orders = {
+        '1' :
         {
             "order_id" : 1,
             "food_ordered": "Fries",
             "quantity" : 4,
             "date_ordered" : "05/09/2018"
         },
+        '2' :
         {
             "order_id" : 2,
             "food_ordered": "Burger",
             "quantity" : 5,
             "date_ordered" : "04/09/2018"
         }
-    ]
+    }
 
     return orders
